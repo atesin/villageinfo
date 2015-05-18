a4532cb7ee4e50d39d6c902a9a54852e21fa340c
atesin <atesin@gmail.com>
2015-05-18 14:07:58 -0400

    v4.183 optimized code and output

--- VillageInfo/src/cl/netgamer/villageinfo/VI.java
+++ VillageInfo/src/cl/netgamer/villageinfo/VI.java
@@ -45,7 +45,7 @@ public final class VI extends JavaPlugin
 		this.saveDefaultConfig();
 		usePermissions = getConfig().getBoolean("usePermissions");
 		lang = getConfig().getConfigurationSection("msg").getValues(false);
-		VI.log("using permissions = "+usePermissions);		
+		VI.log("Using permissions: "+usePermissions);		
 		
 		// no events just 1 command below
 		
@@ -61,6 +61,8 @@ public final class VI extends JavaPlugin
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args)
 	{
+		// previous checks
+		
 		if (!(cmd.getName().equalsIgnoreCase("villageinfo")))
 		{
 			// not "villageinfo" command
@@ -76,17 +78,13 @@ public final class VI extends JavaPlugin
 		Player pla = (Player)sender;
 		
 		//VI.log("usePerm, hasPerm, isOp = "+usePermissions+pla.hasPermission("villageinfo.use")+pla.isOp());
-		if (!usePermissions || pla.hasPermission("villageinfo.use") || pla.isOp()){
-			sender.sendMessage(insideVillageText(pla));
+		if (usePermissions && !pla.hasPermission("villageinfo.use") && !pla.isOp()){
+			pla.sendMessage("§D"+msg("forbidden"));
 			return true;
 		}
 		
-		pla.sendMessage("§D"+msg("forbidden"));
-		return true;
-	}
-	
-	private String insideVillageText(Player pla)
-	{
+		// executing command
+		
 		// guess if player is inside a village
 		// get villages list for this world first
 		Location loc = pla.getLocation();
@@ -101,55 +99,48 @@ public final class VI extends JavaPlugin
 			rad = VillageUtil.getRadius(vil);
 			if (loc.distanceSquared(cen) <= (rad * rad))
 			{
-				String out = "\n§ECenter: ";
-				out += (cen.getBlockX()<0 ? "" : "+")+cen.getBlockX()+(cen.getBlockY()<0 ? "" : "+")+cen.getBlockY()+(cen.getBlockZ()<0 ? "" : "+")+cen.getBlockZ();
-				out += ", Radius: "+rad+"\n"+villageText(vil, pla);
+				/*
+				SAMPLE OUTPUT:
+												// new line
+				Center: +234+54-324, Radius: 32
+				Houses: 8						// red=not enough houses for golems, blue=enough
+				Villagers: 7 (max 3)			// red=no mating, blue=mating
+				Golems: 2 (max 3)				// blue=inside spawn
+				Reputation: 4					// red=hated, blue=loved
+				*/
 				
-				if (Math.abs(loc.getBlockX() - cen.getBlockX()) <= 8 && Math.abs(loc.getBlockY() - cen.getBlockY()) <= 3 && Math.abs(loc.getBlockZ()-cen.getBlockZ()) <= 8)
-				{
-					out += ", Inside Golem Spawn Area";
-				}
+				// just for more readability
+				int cx = cen.getBlockX();
+				int cy = cen.getBlockY();
+				int cz = cen.getBlockZ();
+				int doors = VillageUtil.getNumDoors(vil);
+				int people = VillageUtil.getNumVillagers(vil);
+				int maxPeople = (int)(doors * 0.35D);
+				int golems = VillageUtil.getNumGolems(vil, worlds.get(pla.getLocation().getWorld().getName()));
+				int maxGolems = people / 10;
+				int rep = VillageUtil.getReputationForPlayer(vil, pla);
+				boolean enoughDoors = (doors > 20); // cyan|magenta
+				boolean lowRep = VillageUtil.isPlayerReputationTooLow(vil, pla); // magenta|cyan
+				boolean breeding = VillageUtil.isMatingSeason(vil);
+				boolean insideGolems = Math.abs(loc.getBlockX()-cx)<=8 && Math.abs(loc.getBlockY()-cy)<=3 && Math.abs(loc.getBlockZ()-cz)<=8;
 				
-				return out;
+				// §B=cyan, §D=magenta, §E=yellow
+				// § = alt+21 (win), save as ansi
+				pla.sendMessage("§E"+msg("center")+": "+(cx<0?"":"+")+cx+(cy<0?"":"+")+cy+(cz<0?"":"+")+cz+", "+msg("radius")+": "+rad);
+				pla.sendMessage((enoughDoors?"§B":"§D")+msg("houses")+": "+doors);
+				pla.sendMessage((breeding?"§B":"§D")+msg("villagers")+": "+people+" ("+msg("max")+" "+maxPeople+")");
+				pla.sendMessage((insideGolems?"§B":"§E")+msg("golems")+": "+golems+" ("+msg("max")+" "+maxGolems+")");
+				pla.sendMessage((lowRep?"§D":"§B")+msg("reputation")+": "+rep);
+				return true;
 			}
 		}
 		
-		return "§D"+msg("notInVillage");
+		// not inside a village
+		pla.sendMessage("§D"+msg("notInVillage"));
+		return true;
 	}
 	
-	private String villageText(Village vil, Player pla)
-	{
-		/*
-		Example from Village Info mod ttp://chunkbase.com/mods/village-info
-		
-		Villagers: XX (YY)
-		Golems: XX (YY)
-		Houses: XX [red|green]
-		Your reputation: XX [green|red]
-		Breeding: OK|Stopped
-		*/
-		int people = VillageUtil.getNumVillagers(vil);
-		int doors = VillageUtil.getNumDoors(vil);
-		int breedLimit = (int)(doors * 0.35D);
-		int golems = VillageUtil.getNumGolems(vil, worlds.get(pla.getLocation().getWorld().getName()));
-		int golemLimit = people / 10;
-		boolean enoughDoors = (doors > 20); // cyan|magenta
-		int rep = VillageUtil.getReputationForPlayer(vil, pla);
-		boolean lowRep = VillageUtil.isPlayerReputationTooLow(vil, pla); // magenta|cyan
-		boolean breed = VillageUtil.isMatingSeason(vil); // OK|Stopped
-		
-		// §B=cyan, §D=magenta, §E=yellow
-		// § = alt+21 (win), save as ansi
-		String out = "Villagers: "+people+" ("+breedLimit+"), ";
-		out += "Golems: "+golems+" ("+golemLimit+")\n";
-		out += (enoughDoors ? "§B" : "§D")+"Houses: "+doors+"§E, ";
-		out += (lowRep ? "§D" : "§B")+"Your Reputation: "+rep+"§E\n";
-		out += msg("breedStatus")+": "+(breed ? msg("ok") : msg("Stopped"));
-		
-		return out;
-	}
-	
-    private List<Village> getVillagesByWorldName(String w)
+	private List<Village> getVillagesByWorldName(String w)
     {
     	Object obj = null;
     	WorldServer world = worlds.get(w);
@@ -168,8 +159,7 @@ public final class VI extends JavaPlugin
     	return ((PersistentVillage)obj).getVillages();
     }
 
-	// reflection methods
-	
+	// reflection method
     // returns specified class field, recursively to superclass if implements interface
 	private Field getClassField(Class<?> cls, String field) throws NoSuchFieldException
 	{
